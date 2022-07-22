ccc2cafe2277f478be3e9b01c9b0fe955120050a
yunasugimoto <yunasugimoto@microsoft.com>
2022-07-22 13:46:41 +0900

    sample

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const { executeSQLquery } = require('../sqltest');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource!');
+  res.send('respond with a resource!!');
 });
 
 
